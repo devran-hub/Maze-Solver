8faa03c48b379a272fdfd75d5b9978df4f5f57aa
Devrim Polat <73471656+devran-hub@users.noreply.github.com>
2024-09-15 18:00:24 +0300

    Forward method has been modified with enabled pins

--- MazeSolver.ino
+++ MazeSolver.ino
@@ -1,13 +1,15 @@
-const int trigPin1 = 6;  //front
-const int echoPin1 = 7;
-const int trigPin2 = 10;  //left
-const int echoPin2 = 11;
-const int trigPin3 = 8;  //right
-const int echoPin3 = 9;
-const int in1 = 2;
-const int in2 = 3;
-const int in3 = 4;
-const int in4 = 5;
+#define trigPin1 6  //front
+#define echoPin1 7
+#define trigPin2 10  //left
+#define echoPin2 11
+#define trigPin3 8  //right
+#define echoPin3 9
+#define in1 2
+#define in2 3
+#define in3 4
+#define in4 5
+#define ENL 12
+#define ENR 13
 
 #define DIS 5
 
@@ -24,7 +26,14 @@ void setup() {
   pinMode(in4, OUTPUT);
 }
 void loop() {
-  if (FrontSensor() > 17 && RightSensor() > DIS && LeftSensor() > DIS) { forward(); }
+  analogWrite(ENR, 255);
+  analogWrite(ENL, 255);
+
+  if (FrontSensor() > 17 && RightSensor() > DIS && LeftSensor() > DIS) {
+    turn_right();  // then turn  right and then forward
+    delay(500);
+    forward();
+  }
   //else if ( FrontSensor () > DIS && RightSensor () <  DIS && LeftSensor ()< DIS)
   //{
   //forward();
@@ -37,22 +46,23 @@ void loop() {
       turn_left();
     else
       turn_right();
-    //then  reverse
+
+    delay(200);
   } else if (FrontSensor() < 18 && RightSensor() < DIS && LeftSensor() > DIS)  // obstacle on right and front sides
   {
     turn_left();
     delay(500);
-    forward();                                                                  // turn left  side
+    forward();                                                                 // turn left  side
   } else if (FrontSensor() < 17 && RightSensor() > DIS && LeftSensor() < DIS)  // obstacle on left and front sides
   {
-    turn_right();  
+    turn_right();
     delay(500);
-    forward();                                                             // turn right side
+    forward();                                                                 // turn right side
   } else if (FrontSensor() < 17 && RightSensor() > DIS && LeftSensor() > DIS)  // obstacle  on front sides
   {
-    turn_left();
+    turn_right();
     delay(500);
-    forward();                                                                  // then turn right  //********************
+    forward();                                                                 // then turn right  //********************
   } else if (FrontSensor() > 17 && RightSensor() > DIS && LeftSensor() < DIS)  // obstacle on left sides
   {
     turn_right();  // then turn  right and then forward
@@ -60,8 +70,6 @@ void loop() {
     forward();
   } else if (FrontSensor() > 17 && RightSensor() < DIS && LeftSensor() > DIS)  // obstacle on right sides
   {
-    turn_left();  // then turn left and then right
-    delay(500);
     forward();
   } else {
     forward();
@@ -72,7 +80,13 @@ void forward() {
   digitalWrite(in2, LOW);
   digitalWrite(in3, HIGH);
   digitalWrite(in4, LOW);
+  //distances has been rounded to obtain smoother movement.
+  if ((int)RightSensor() > (int)LeftSensor()) analogWrite(ENR, 150);  //We don't need to specifiy speed of the other motor because we already define it at at the beginning of the loop
+  else if ((int)RightSensor() < (int)LeftSensor()) analogWrite(ENL, 150);
+  delay(200);
 }
+
+
 void turn_left() {
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
